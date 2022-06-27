e871b0ecbd309a55d0ae5b64089648c3242504a3
nome <email@test.com>
2022-06-27 12:21:54 -0300

    Encapsulamento

--- src/Caixa.java
+++ src/Caixa.java
@@ -1,4 +1,40 @@
 
 public class Caixa {
+	private int altura;
+	private int largura;
+	private int profundidade;
 
+	public void cabeDentro(int diametro) {
+		if (diametro <= largura && diametro <= altura && diametro <= profundidade) {
+			System.out.println("Cabe.");
+		}
+		else {
+			System.out.println("Não cabe.");
+		}
+	}
+	
+	
+	public int getAltura() {
+		return altura;
+	}
+
+
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+
+
+	public int getLargura() {
+		return largura;
+	}
+	public void setLargura(int largura) {
+		this.largura = largura;
+	}
+	public int getProfundidade() {
+		return profundidade;
+	}
+	public void setProfundidade(int profundidade) {
+		this.profundidade = profundidade;
+	}
+	
 }
--- src/TestaCaixa.java
+++ src/TestaCaixa.java
@@ -1,4 +1,15 @@
 
 public class TestaCaixa {
-
+	
+	public static void main(String[] args) {
+		int diametro = 3;
+		
+		Caixa c = new Caixa();
+		c.setLargura(3);
+		c.setProfundidade(5);
+		c.setAltura(2);
+		
+		c.cabeDentro(diametro);
+	}
+	
 }
